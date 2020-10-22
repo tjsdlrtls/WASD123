0fe1011fa0f7acbd223c6817d04011b5e2971318
이름 <>
2020-10-22 15:14:42 +0900

    add

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on("guildMemberAdd", (member) => {
 
   welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
 
-  member.addRole(guild.roles.find(role => role.name == "게스트"));
+  member.addRole(guild.roles.find(role => role.name == "맴버"));
 });
 
 client.on("guildMemberRemove", (member) => {
